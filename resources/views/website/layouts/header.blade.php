d8cfc1b4ebd9e3b634a7f7f34d3c54a776ed83b1
Wael Moharram <wael.moharram@gmail.com>
2022-11-18 22:57:57 +0200

    Social media header

--- resources/views/website/layouts/header.blade.php
+++ resources/views/website/layouts/header.blade.php
@@ -30,12 +30,12 @@
                 <!-- Top Social
                 ============================================= -->
                 <ul id="top-social">
-                    <li><a href="https://facebook.com/semicolonweb" class="si-facebook" target="_blank"><span class="ts-icon"><i class="icon-facebook"></i></span><span class="ts-text">Facebook</span></a></li>
-                    <li><a href="https://twitter.com/__semicolon" class="si-twitter" target="_blank"><span class="ts-icon"><i class="icon-twitter"></i></span><span class="ts-text">Twitter</span></a></li>
-                    <li><a href="https://youtube.com/semicolonweb" class="si-youtube" target="_blank"><span class="ts-icon"><i class="icon-youtube"></i></span><span class="ts-text">Youtube</span></a></li>
-                    <li><a href="https://instagram.com/semicolonweb" class="si-instagram" target="_blank"><span class="ts-icon"><i class="icon-instagram2"></i></span><span class="ts-text">Instagram</span></a></li>
-                    <li><a href="tel:+10.11.85412542" class="si-call"><span class="ts-icon"><i class="icon-call"></i></span><span class="ts-text">+10.11.85412542</span></a></li>
-                    <li><a href="mailto:info@canvas.com" class="si-email3"><span class="ts-icon"><i class="icon-envelope-alt"></i></span><span class="ts-text">info@canvas.com</span></a></li>
+                    <li><a href="{{option('facebook')}}" class="si-facebook" target="_blank"><span class="ts-icon"><i class="icon-facebook"></i></span><span class="ts-text">Facebook</span></a></li>
+                    <li><a href="{{option('twitter')}}" class="si-twitter" target="_blank"><span class="ts-icon"><i class="icon-twitter"></i></span><span class="ts-text">Twitter</span></a></li>
+                    <li><a href="{{option('youtube')}}" class="si-youtube" target="_blank"><span class="ts-icon"><i class="icon-youtube"></i></span><span class="ts-text">Youtube</span></a></li>
+                    <li><a href="{{option('instagram')}}" class="si-instagram" target="_blank"><span class="ts-icon"><i class="icon-instagram2"></i></span><span class="ts-text">Instagram</span></a></li>
+                    <li><a href="tel:{{option('phone')}}" class="si-call"><span class="ts-icon"><i class="icon-call"></i></span><span class="ts-text">{{option('phone')}}</span></a></li>
+                    <li><a href="mailto:{{option('email')}}" class="si-email3"><span class="ts-icon"><i class="icon-envelope-alt"></i></span><span class="ts-text">{{option('email')}}</span></a></li>
                 </ul><!-- #top-social end -->
 
             </div>

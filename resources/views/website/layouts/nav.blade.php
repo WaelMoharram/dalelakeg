0115a3b0101bb6d79616640484f018bae8e57e36
Wael Moharram <wael.moharram@gmail.com>
2023-02-25 02:35:20 +0200

    Images

--- resources/views/website/layouts/nav.blade.php
+++ resources/views/website/layouts/nav.blade.php
@@ -5,17 +5,7 @@
     <ul class="menu-container">
         @if(app()->getLocale() == 'en')
         <li class="menu-item @if(Request::is('/')) current  @endif"><a class="menu-link" href="{{route('home')}}"><div>{{__('Home')}}</div></a></li>
-        <li class="menu-item"><a class="menu-link" href="@if(app()->getLocale() == 'ar')
-            {{route('blog',1)}}
-            @else
-            {{route('blog',2)}}
-            @endif"><div>
-                    @if(app()->getLocale() == 'ar')
-                        {{\App\Models\Blog::find(1)->title}}
-                    @else
-                        {{\App\Models\Blog::find(2)->title}}
-                    @endif
-                </div></a></li>
+
         <li class="menu-item mega-menu"><div class="menu-link"><div>{{__('Services')}}</div></div>
             <div class="mega-menu-content mega-menu-style-2 px-0">
                 <div class="container">
@@ -40,13 +30,41 @@
                 </div>
             </div>
         </li>
+            <li class="menu-item @if(Request::is('images')) current  @endif"><a class="menu-link" href="{{route('images')}}"><div>{{__('Images')}}</div></a></li>
+
         <li class="menu-item @if(Request::is('faqs')) current  @endif"><a class="menu-link" href="{{route('faqs')}}"><div>{{__('FAQs')}}</div></a></li>
+
+            <li class="menu-item"><a class="menu-link" href="@if(app()->getLocale() == 'ar')
+                {{route('blog',1)}}
+                @else
+                {{route('blog',2)}}
+                @endif"><div>
+                        @if(app()->getLocale() == 'ar')
+                            {{\App\Models\Blog::find(1)->title}}
+                        @else
+                            {{\App\Models\Blog::find(2)->title}}
+                        @endif
+                    </div></a></li>
+
         <li class="menu-item"><a class="menu-link" target="_blank" href="https://wa.me/{{option('whatsapp')}}"><div>{{__('Contact')}}</div></a></li>
         @else
             <li class="menu-item"><a class="menu-link" target="_blank" href="https://wa.me/{{option('whatsapp')}}"><div>{{__('Contact')}}</div></a></li>
 
-            <li class="menu-item @if(Request::is('faqs')) current  @endif"><a class="menu-link" href="{{route('faqs')}}"><div>{{__('FAQs')}}</div></a></li>
+            <li class="menu-item"><a class="menu-link" href="@if(app()->getLocale() == 'ar')
+                {{route('blog',1)}}
+                @else
+                {{route('blog',2)}}
+                @endif"><div>
+                        @if(app()->getLocale() == 'ar')
+                            {{\App\Models\Blog::find(1)->title}}
+                        @else
+                            {{\App\Models\Blog::find(2)->title}}
+                        @endif
+                    </div></a>
+            </li>
 
+            <li class="menu-item @if(Request::is('faqs')) current  @endif"><a class="menu-link" href="{{route('faqs')}}"><div>{{__('FAQs')}}</div></a></li>
+            <li class="menu-item @if(Request::is('images')) current  @endif"><a class="menu-link" href="{{route('images')}}"><div>{{__('Images')}}</div></a></li>
 
             <li class="menu-item mega-menu"><div class="menu-link"><div>{{__('Services')}}</div></div>
                 <div class="mega-menu-content mega-menu-style-2 px-0">
@@ -73,17 +91,7 @@
                 </div>
             </li>
 
-            <li class="menu-item"><a class="menu-link" href="@if(app()->getLocale() == 'ar')
-                {{route('blog',1)}}
-                @else
-                {{route('blog',2)}}
-                @endif"><div>
-                        @if(app()->getLocale() == 'ar')
-                            {{\App\Models\Blog::find(1)->title}}
-                        @else
-                            {{\App\Models\Blog::find(2)->title}}
-                        @endif
-                    </div></a></li>
+
 
 
             <li class="menu-item @if(Request::is('/')) current  @endif"><a class="menu-link" href="{{route('home')}}"><div>{{__('Home')}}</div></a></li>

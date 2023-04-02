71fb3033673120c088d4bf339abb0271cb73d8d6
Wael Moharram <wael.moharram@gmail.com>
2023-04-02 04:06:39 +0200

    googel script

--- resources/views/website/layouts/scripts.blade.php
+++ resources/views/website/layouts/scripts.blade.php
@@ -8,3 +8,13 @@
 <script src="{{asset('assets/website')}}/js/functions.js"></script>
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.js" integrity="sha512-gY25nC63ddE0LcLPhxUJGFxa2GoIyA5FLym4UJqHDEMHjp8RET6Zn/SHo1sltt3WuVtqfyxECP38/daUc/WVEA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-RWDLMYPZKZ"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-RWDLMYPZKZ');
+</script>

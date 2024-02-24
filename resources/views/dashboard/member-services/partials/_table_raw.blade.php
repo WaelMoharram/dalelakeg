d94cf8eefda720dfafd4249eaaef99773f973141
Wael Moharram <wael.moharram@gmail.com>
2024-02-24 20:25:26 +0200

    logo + members DB

--- resources/views/dashboard/member-services/partials/_table_raw.blade.php
+++ resources/views/dashboard/member-services/partials/_table_raw.blade.php
@@ -1,21 +1,21 @@
 <tr>
     <td>{!! $loop->index +1 !!}</td>
 {{--    <td><img src="{!! url('/').'/'.$member->image !!}" style="width: 100px; height: 100px;"></td>--}}
-    <td>{!! $member->title !!}</td>
+    <td>{!! $memberService->title !!}</td>
     <td>
         <div class="btn-group" role="group" aria-label="Vertical button group">
 
             <div class="btn-group" role="group">
                 @component('dashboard.layouts.partials.buttons._edit_button',[
-                        'route' => route('dashboard.members.edit',$member->id),
+                        'route' => route('dashboard.members.edit',$memberService->id),
                         'tooltip' => __('Edit members'),
                          ])
                 @endcomponent
             </div>
             <div class="btn-group" role="group">
                 @component('dashboard.layouts.partials.buttons._delete_button',[
-                            'id'=>$member->id,
-                            'route' => route('dashboard.members.destroy',$member->id) ,
+                            'id'=>$memberService->id,
+                            'route' => route('dashboard.members.destroy',$memberService->id) ,
                             'tooltip' => __('Delete members'),
                              ])
                 @endcomponent
